80ff3485fdbcb0225268edaf8fed7e717ff1cd2e
Elvin <elvinhaxverdiyev777@gamil.com>
2025-02-02 21:52:27 +0400

    fixed form bug

--- porfolio/core/forms.py
+++ porfolio/core/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from .models import Contact
 
 class ContactForm(forms.ModelForm):
+    name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
     class Meta:
         model = Contact
         fields = ["name", "email", "message"]
